256f37c91aa6aa19aa8a5a3d406914f781299729
mrgrin93 <agrishin@Peka>
2022-12-20 09:11:26 +0300

    fix

--- read_sms.py
+++ read_sms.py
@@ -1,52 +1,52 @@
-# import configparser
-# import binascii
+import configparser
+import binascii
 
-# # Create a ConfigParser instance
-# config = configparser.ConfigParser()
+# Create a ConfigParser instance
+config = configparser.ConfigParser()
 
-# # Read the INI file
-# config.read('/var/spool/gammu/inbox/IN20221220_141717_00_Beeline^M_00.txt')
+# Read the INI file
+config.read('/var/spool/gammu/inbox/IN20221220_141717_00_Beeline^M_00.txt')
 
-# # Create an empty dictionary to store the data
-# data = {}
+# Create an empty dictionary to store the data
+data = {}
 
-# # Iterate over the sections in the INI file
-# for section in config.sections():
-#   # Create a dictionary to store the keys and values in the section
-#   section_data = {}
+# Iterate over the sections in the INI file
+for section in config.sections():
+  # Create a dictionary to store the keys and values in the section
+  section_data = {}
   
-#   # Iterate over the keys and values in the section
-#   for key, value in config[section].items():
-#     # Add the key and value to the section data
-#     section_data[key] = value
+  # Iterate over the keys and values in the section
+  for key, value in config[section].items():
+    # Add the key and value to the section data
+    section_data[key] = value
     
-#   # Add the section data to the main dictionary
-#   data[section] = section_data
+  # Add the section data to the main dictionary
+  data[section] = section_data
 
-# # The data dictionary now contains the contents of the INI file
-# print(data)
+# The data dictionary now contains the contents of the INI file
+print(data)
 
-# sms = ''
+sms = ''
 
-# for i in data.keys():
-#     for k in data[i].keys():
-#         if 'text' in k:
-#             print(data[i][k])
-#             # smsb = bytes.fromhex(data[i][k])
-#             sms =  bytes.fromhex(data[i][k]).decode('utf-8')
+for i in data.keys():
+    for k in data[i].keys():
+        if 'text' in k:
+            print(data[i][k])
+            # smsb = bytes.fromhex(data[i][k])
+            sms =  bytes.fromhex(data[i][k]).decode('utf-32-le')
 
 
-# print(sms)
+print(sms)
+# msg = ''
+# with open('/var/spool/gammu/inbox/IN20221220_141717_00_Beeline^M_00.txt', 'r') as f:
+#     # Initialize a list to store the comments
+#     comments = []
+#     # Read the file line by line
+#     for line in f:
+#         # Check if the line starts with a semicolon
+#         if line.startswith(';'):
+#             # The line is a comment, so remove the leading semicolon and store it
+#             comments.append(line[1:])
+#     msg = ''.join(comments[3:])
 
-with open('/var/spool/gammu/inbox/IN20221220_141717_00_Beeline^M_00.txt', 'r') as f:
-    # Initialize a list to store the comments
-    comments = []
-    # Read the file line by line
-    for line in f:
-        # Check if the line starts with a semicolon
-        if line.startswith(';'):
-            # The line is a comment, so remove the leading semicolon and store it
-            comments.append(line[1:])
-        
-
-print(''.join(comments[3:]))
+# print(''.join(comments[3:]))
